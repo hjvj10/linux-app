444cfe99f45a90b9a5866426ae386a8fc648d210
Alexandru Cheltuitor <calexandru2018@pm.me>
2021-02-03 18:25:23 +0000

    Add possibility to show/hide password

--- linux_app/assets/windows/login.py
+++ linux_app/assets/windows/login.py
@@ -3,7 +3,7 @@ import os
 import gi
 gi.require_version('Gtk', '3.0')
 
-from gi.repository import Gtk, GdkPixbuf, Gdk
+from gi.repository import Gtk, GdkPixbuf, Gdk, Gio
 
 from ...constants import CSS_DIR_PATH, UI_DIR_PATH, IMG_DIR_PATH, ICON_DIR_PATH
 
@@ -15,6 +15,28 @@ class LoginWindow(Gtk.ApplicationWindow):
     proton_password_entry = Gtk.Template.Child()
     login_button = Gtk.Template.Child()
     img_protonvpn_logo = Gtk.Template.Child()
+    icon_width = 18
+    icon_heigt = 18
+    password_show_entry_pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
+        filename=os.path.join(
+            ICON_DIR_PATH,
+            "eye-show.imageset/eye-show@3x.png",
+
+        ),
+        width=icon_width,
+        height=icon_heigt,
+        preserve_aspect_ratio=True
+    )
+    password_hide_entry_pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
+        filename=os.path.join(
+            ICON_DIR_PATH,
+            "eye-hide.imageset/eye-hide@3x.png",
+
+        ),
+        width=icon_width,
+        height=icon_heigt,
+        preserve_aspect_ratio=True
+    )
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -27,26 +49,21 @@ class LoginWindow(Gtk.ApplicationWindow):
                 IMG_DIR_PATH,
                 "protonvpn-logo-white.svg"
             ),
-            width=300,
-            height=200,
-            preserve_aspect_ratio=True
-        )
-        password_entry_pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
-            filename=os.path.join(
-                ICON_DIR_PATH,
-                "eye-show.imageset/eye-show@2x.png",
-
-            ),
-            width=15,
-            height=15,
+            width=325,
+            height=250,
             preserve_aspect_ratio=True
         )
 
         self.img_protonvpn_logo.set_from_pixbuf(logo_pixbuf)
         self.proton_password_entry.set_icon_from_pixbuf(
             Gtk.EntryIconPosition.SECONDARY,
-            password_entry_pixbuf
+            self.password_show_entry_pixbuf
         )
+        self.proton_password_entry.set_icon_activatable(
+            Gtk.EntryIconPosition.SECONDARY,
+            True
+        )
+        self.proton_password_entry.connect("icon-press", self.set_password_visibility)
 
     def setup_css(self):
         self.provider = Gtk.CssProvider()
@@ -57,5 +74,18 @@ class LoginWindow(Gtk.ApplicationWindow):
             self.provider,
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
+
+    def set_password_visibility(self, entry_object, icon_object, event):
+        is_text_visible = entry_object.get_visibility()
+        entry_object.set_visibility(not is_text_visible)
+        pixbuf = (
+            self.password_show_entry_pixbuf
+            if is_text_visible
+            else self.password_hide_entry_pixbuf)
+        self.proton_password_entry.set_icon_from_pixbuf(
+            Gtk.EntryIconPosition.SECONDARY,
+            pixbuf
+        )
+
 
 
