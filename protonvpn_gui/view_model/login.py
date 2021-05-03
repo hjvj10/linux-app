578ee082e945a14058a74b41a24b2c0ef510be7c
alexandru <calexandru2018@pm.me>
2021-05-03 12:33:50 +0100

    Displays appropriate error messaged if login fails

--- protonvpn_gui/view_model/login.py
+++ protonvpn_gui/view_model/login.py
@@ -5,6 +5,7 @@ from protonvpn_nm_lib import exceptions
 from ..rx.subject.replaysubject import ReplaySubject
 from ..logger import logger
 from protonvpn_nm_lib.enums import KillswitchStatusEnum
+from proton import exceptions as proton_excp
 
 
 class LoginState(Enum):
@@ -36,9 +37,37 @@ class LoginViewModel:
         try:
             protonvpn.login(username, password)
             result = LoginState.SUCCESS
+        except proton_excp.TLSPinningError as e:
+            logger.exception(e)
+            result = LoginError(
+                "Your connection is not secure. "
+                "Please change network and attempt a new connection."
+                )
+        except proton_excp.NewConnectionError as e:
+            logger.exception(e)
+            result = LoginError(
+                "Unable to establish a new connection. "
+                "Please ensure that you have internet connection.\n"
+                "If the issue persists, please contact support."
+            )
+        except proton_excp.UnknownConnectionError as e:
+            logger.exception(e)
+            result = LoginError(
+                "Unknown connection error. "
+                "If the issue persits, pleaese contact support."
+            )
+        except proton_excp.ProtonError as e:
+            logger.exception(
+                "code: {} - error: {} - headers: {}".format(
+                    e.code, e.error, e.headers
+                )
+            )
+            result = LoginError("{}".format(str(e.error)))
         except (exceptions.ProtonVPNException, Exception) as e:
             logger.exception(e)
-            result = LoginError("{}".format(str(e)))
+            result = LoginError(
+                "Unknown error occured. If the issue persists, please contact support."
+            )
 
         self.state.on_next(result)
 
